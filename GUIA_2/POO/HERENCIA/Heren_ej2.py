9cca00f269566d0709e5bb4137f6be1d5b30c9e0
Caf3enJ4rrit0 <caf3enj4rrit0@gmail.com>
2020-10-12 16:24:31 -0300

    Herencia ej2 finalizado

--- GUIA_2/POO/HERENCIA/Heren_ej2.py
+++ GUIA_2/POO/HERENCIA/Heren_ej2.py
@@ -23,48 +23,46 @@ class Persona:
         self.edad = edad
         self.dni = dni
 
-class Titular(Persona):
-    def __init__(self, nombre, edad, dni, mail):
-        super().__init__(nombre, edad, dni)
-        self.mail = mail
+    def muestra(self):
+        print(f"Nombre: {self.nombre}, Edad: {self.edad}, DNI: {self.dni}")
 
-class Cuenta(Titular):
 
-    def __init__(self, nombre, edad, dni, mail, titular, cantidad):
-        super().__init__(nombre, edad, dni, mail)
-        self.titular = titular                      #clase llamada Cuenta que tendrá los siguientes atributos: titular (que es una persona) NO ME SALE
-        self.cantidad = cantidad
+
+class Cuenta:
+    def __init__(self, titular_nombre, titular_edad, titular_dni, cantidad = 0):
+        self.tit_nom = titular_nombre                      #clase llamada Cuenta que tendrá los siguientes atributos: titular (que es una persona) NO ME SALE
+        self.tit_edad = titular_edad
+        self.tit_dni = titular_dni
+        self.__cantidad = cantidad
 
 
     def mostrar(self):
-        print(f"DATOS PERSONALES: \nTITULAR: {self.nombre} \nEDAD: {self.edad} \nDNI: {self.dni} \nMAIL: {self.mail}")
-        print(f"SALDO: ${self.cantidad}")
+        print(f"DATOS TITULAR: \nNOMBRE COMPLETO: {self.tit_nom}, \nEDAD: {self.tit_edad}, \nDNI:{self.tit_dni}")
+        print(f"SALDO: ${self.__cantidad}")
 
     def ingresar(self):
         ingresa = int(input("Desea ingresar dinero a la cuenta? S=1/N=0: "))
-        print(ingresa)
         if ingresa == 1:
             monto = int(input("ingresar el monto a depositar: "))
             if monto != 0:
-                self.cantidad += monto
+                self.__cantidad += monto
             elif monto == 0:
                 print("no ingreso un monto invalido")
                 print(ingresa)
         elif ingresa == 0:
             print("gracias por usar nuestros servicios")
-        elif ingresa != 1 and ingresa!= 0:
+        elif ingresa != 1 and ingresa != 0:
             print("error")
 
     def retirar(self):
         retira = int(input("Desea retirar dinero de la cuenta? S=1/N=0: "))
-        print(retira)
         if retira == 1:
             valor = int(input("ingresar el monto a retirar: "))
             if valor != 0:
-                self.cantidad -= valor
-                if self.cantidad < 0:
+                self.__cantidad -= valor
+                if self.__cantidad < 0:
                     print("la cuenta esta en numeros rojos")
-                    print(f"Su saldo: {self.cantidad}")
+                    print(f"Su saldo: {self.__cantidad}")
             elif valor == 0:
                 print("no ingreso un monto invalido")
                 print(retira)
@@ -73,12 +71,28 @@ class Cuenta(Titular):
         elif ingresa != 1 and ingresa != 0:
             print("error")
 
+t_nom = input("ingrese su nombre completo: ")
+t_edad = int(input("ingrese su edad: "))
+t_dni = int(input("ingrese su dni: "))
 
-belen = Cuenta("Belen S.", 32, 33589676, "mail@mail.com", "hola", 20000)
+alguien = Persona(t_nom, t_edad, t_dni)                             #Objeto de Persona
+
+p = Cuenta(alguien.nombre, alguien.edad, alguien.dni)               #objeto de Cuenta
+
+p.mostrar()
+p.ingresar()
+p.mostrar()
+p.retirar()
+p.mostrar()
+
+
+
+
+"""belen = Cuenta(titular., 20000)
 
 
 belen.mostrar()
 belen.ingresar()
 belen.mostrar()
 belen.retirar()
-
+"""
